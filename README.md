d522cc353540b221cacc6c0078ea7dca4cf825e8
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-02-19 00:48:03 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Playground repo for learning Pythong
---
+++ src/data_types.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def just(t):
+    return str(t).ljust(20)
+
+def printType(t):
+    print("%s is of %s" % (just(t), type(t)) ) 
+
+printType(False)
+printType(1)
+printType(1.0)
+printType(1L)
+printType("hello world")
+printType([1,2,3])
+printType(("one","two"))
+printType({"Key":"Value"})
+
